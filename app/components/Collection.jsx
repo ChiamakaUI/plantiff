3fc033cd0e50b2c26d2a790b84eb3f70b0870f30
ChiamakaUI <55190142+ChiamakaUI@users.noreply.github.com>
2023-08-04 01:06:35 +0100

    removed strapi functions completely

--- app/components/Collection.jsx
+++ app/components/Collection.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { urlFor } from '../lib/sanityImageUrl';
 
 const Collection = ({ lgStyles, name, image }) => {
     return (
@@ -6,7 +7,7 @@ const Collection = ({ lgStyles, name, image }) => {
             className={`relative w-[95%] h-[225px] mx-auto my-2 ${lgStyles} text-white lg:w-auto lg:h-auto lg:mx-0 lg:my-0 md:w-auto md:h-auto md:mx-0 md:my-0`}
         >
             <Image
-                src={`http://127.0.0.1:1337${image}`}
+                src={urlFor(image).url()}
                 alt="plants"
                 width={150}
                 height={150}
--- app/components/Collections.jsx
+++ app/components/Collections.jsx
@@ -1,9 +1,16 @@
 import Collection from "./Collection";
-import getCategories from "@/lib/getCategories";
+import { client } from '../lib/sanity';
 
+
+async function getCategories() {
+  const query = `*[_type == "category"]`;
+  const data = await client.fetch(query);
+
+  return data;
+}
 const Collections = async () => {
-  const res = await getCategories();
-  const { data } = res;
+  const data = await getCategories();
+
   return (
     <div className="font-serif w-[98%] mx-auto lg:w-[78%] md:w-[88%] lg:h-[918px] md:h-[600px]">
       <p className="text-center text-3xl lg:text-5xl lg:font-semibold lg:my-4 dark:text-white">
@@ -12,8 +19,8 @@ const Collections = async () => {
       <div className="flex flex-col lg:grid md:grid grid-rows-3 grid-cols-3 gap-3 md:gap-2">
         {data.map((cat, index) => (
           <Collection
-            key={cat.id}
-            name={cat.attributes.name}
+            key={cat._id}
+            name={cat.name}
             lgStyles={
               index === 0
                 ? "lg:col-span-2 lg:row-span-1 md:col-span-2 md:row-span-1"
@@ -21,7 +28,7 @@ const Collections = async () => {
                 ? "lg:col-span-2 lg:row-span-1 md:col-span-2 md:row-span-1"
                 : "lg:col-span-1 md:col-span-1"
             }
-            image={cat.attributes.image.data[0].attributes.url}
+            image={cat.image}
           />
         ))}
       </div>
